93a6930ebcbab14d9a4a1c8541afd25649e124f6
Saim <majesticpainter79@gmail.com>
2026-02-19 16:28:46 +0500

    port lowercase

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const { Resend } = require('resend');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -109,6 +109,6 @@ app.post('/newsletter', async (req, res) => {
     }
 });
 
-app.listen(PORT, '0.0.0.0' , () => {
-    console.log(`Backend is running on port ${PORT}`);
+app.listen(port, '0.0.0.0' , () => {
+    console.log(`Backend is running on port ${port}`);
 });
